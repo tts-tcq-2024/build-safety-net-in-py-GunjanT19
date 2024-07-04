a9a636fa927b3bab08a1a4b65c41e7c74cf3681e
GunjanT19 <78368508+GunjanT19@users.noreply.github.com>
2024-07-04 14:29:34 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -2,41 +2,46 @@ import unittest
 
 class TestSoundex(unittest.TestCase):
 
-    def test_empty_string(self):
+    def test_get_soundex_code(self):
+        self.assertEqual(get_soundex_code('B'), '1')
+        self.assertEqual(get_soundex_code('C'), '2')
+        self.assertEqual(get_soundex_code('D'), '3')
+        self.assertEqual(get_soundex_code('L'), '4')
+        self.assertEqual(get_soundex_code('M'), '5')
+        self.assertEqual(get_soundex_code('R'), '6')
+        self.assertEqual(get_soundex_code('A'), '0')  # Vowel
+        self.assertEqual(get_soundex_code('H'), '0')  # Ignored character
+        self.assertEqual(get_soundex_code('Z'), '2')
+        self.assertEqual(get_soundex_code('x'), '2')  # Lowercase input
+
+    def test_remove_consecutive_duplicates(self):
+        self.assertEqual(remove_consecutive_duplicates("A111223344"), "A1234")
+        self.assertEqual(remove_consecutive_duplicates("A111"), "A1")
+        self.assertEqual(remove_consecutive_duplicates("A"), "A")
+        self.assertEqual(remove_consecutive_duplicates("A1"), "A1")
+        self.assertEqual(remove_consecutive_duplicates(""), "")
+
+    def test_encode_characters(self):
+        self.assertEqual(encode_characters("Robert"), "R163")
+        self.assertEqual(encode_characters("Rupert"), "R163")
+        self.assertEqual(encode_characters("Rubin"), "R150")
+        self.assertEqual(encode_characters("Ashcraft"), "A22631")
+        self.assertEqual(encode_characters("Tymczak"), "T5222")
+        self.assertEqual(encode_characters("Pfister"), "P1236")
+        self.assertEqual(encode_characters(""), "")
+
+    def test_generate_soundex(self):
         self.assertEqual(generate_soundex(""), "")
-
-    def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
-        self.assertEqual(generate_soundex("B"), "B000")
-
-    def test_vowels_ignored(self):
-        self.assertEqual(generate_soundex("AEIOU"), "A000")
-        self.assertEqual(generate_soundex("BOAT"), "B300")
-
-    def test_adjacent_same_code(self):
-        self.assertEqual(generate_soundex("BFPV"), "B100")
-        self.assertEqual(generate_soundex("CGJKQSXZ"), "C220")
-
-    def test_h_w_between_same_code(self):
-        self.assertEqual(generate_soundex("BHW"), "B000")
-        self.assertEqual(generate_soundex("BPV"), "B100")
-        self.assertEqual(generate_soundex("BXZW"), "B200")
-
-    def test_zeros_padding(self):
-        self.assertEqual(generate_soundex("D"), "D000")
-        self.assertEqual(generate_soundex("DL"), "D400")
-
-    def test_length_limited_to_four(self):
-        self.assertEqual(generate_soundex("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "A123")
-
-    def test_real_names(self):
         self.assertEqual(generate_soundex("Robert"), "R163")
         self.assertEqual(generate_soundex("Rupert"), "R163")
         self.assertEqual(generate_soundex("Rubin"), "R150")
         self.assertEqual(generate_soundex("Ashcraft"), "A261")
         self.assertEqual(generate_soundex("Tymczak"), "T522")
         self.assertEqual(generate_soundex("Pfister"), "P236")
+        self.assertEqual(generate_soundex("BOAT"), "B300")
+        self.assertEqual(generate_soundex("BFPV"), "B100")
+        self.assertEqual(generate_soundex("CGJKQSXZ"), "C220")
 
 if __name__ == "__main__":
     unittest.main()
-
